989321f680069391c15004b21e9a1661fa685122
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-09-26 16:21:26 +0800

    Add files via upload

---
+++ c_base/src/songhan.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>    //延时  
+//int i;
+int main()
+{
+   // printf("%d", 4<8);   // 1
+
+
+    // int i;
+    // while(-1)      // 无限循环
+    // {
+    //     if(i<5) continue; //后面的跳过
+    //     i++;
+    //     // sleep(1);
+    //     printf("%d",i);
+    // }
+
+    printf("qqq");
+    for(int i=6;i>3;i--)   //654
+    {
+        printf("%d",i);
+    }
+}
